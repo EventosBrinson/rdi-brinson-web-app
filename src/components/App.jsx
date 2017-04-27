185286c6009b7c6828e767700075737ef6c8e5c6
David De Anda <omarandstuff@gmail.com>
2017-04-26 22:57:21 -0500

    Router scroll to top

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,15 @@
 
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop'
 
 export default class App extends React.Component {
   render() {
     return (
       <Router>
-
+        <ScrollToTop>
+          <div>RDI Brinson</div>
+        </ScrollToTop>
       </Router>
     )
   }
