3030d1de4ba400287b096a83cf91ccfd687bbbf1
David De Anda <omarandstuff@gmail.com>
2017-04-26 22:27:46 -0500

    React router

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+
+      </Router>
+    )
+  }
+}
