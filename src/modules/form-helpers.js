f9b548b02b0d9fd29eda852f7e9e18fe2d6a6a6f
David De Anda <omarandstuff@gmail.com>
2017-05-27 10:46:34 -0500

    Don't pass nulls

--- src/modules/form-helpers.js
+++ src/modules/form-helpers.js
@@ -1,6 +1,6 @@
 export function priorityValues(values) {
   for(var i = 0; i < values.length; i++) {
-    if(values[i] === '' || values[i] !== undefined) {
+    if(values[i] === '' || (values[i] !== undefined && values[i] !== null)) {
       return values[i]
     }
   }
