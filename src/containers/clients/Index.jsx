5527960c003d17e5f83798b618017e807387000a
David De Anda <omarandstuff@gmail.com>
2017-06-13 23:58:49 -0500

    Client index uses client list component

---
+++ src/containers/clients/Index.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
+import * as actionCreators from '../../action-creators'
+import Immutable from 'immutable'
+import ClientList from '../../components/clients/ClientList'
+
+import { Tabs, Button } from 'antd'
+
+class Index extends React.Component {
+
+  componentDidMount() {
+    this.getClients(this.props)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.getClients(nextProps)
+  }
+
+  getClients(props) {
+    if(props.session_status === 'SIGNED_IN' && props.clients.get('get_clients_status') !== 'READY') {
+      props.submitRequest('GET_CLIENTS') 
+    }
+  }
+
+  render() {
+    return (
+      <div style={ { marginTop: '20px'} }>
+        <Button>
+          <Link to="/clients/new">
+            Crear nuevo
+          </Link>
+        </Button>
+        <Tabs>
+          <Tabs.TabPane tab="Activos" key="1">
+            <ClientList active={ true } 
+                        order={ this.props.order || Immutable.List() }
+                        hashed={ this.props.hashed || Immutable.Map() }
+                        submitRequest={ this.props.submitReques }/>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab="Inactivos" key="2">
+            <ClientList active={ false } 
+                        order={ this.props.order || Immutable.List() }
+                        hashed={ this.props.hashed || Immutable.Map()}
+                        submitRequest={ this.props.submitReques }/>
+          </Tabs.TabPane>
+        </Tabs>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    clients: state.get('clients') || Immutable.Map(),
+    hashed: state.getIn(['clients', 'hashed']),
+    order: state.getIn(['clients', 'order']),
+    session_status: state.get('session_status')
+  }
+}
+
+export default withRouter(connect(mapStateToProps, actionCreators)(Index))
