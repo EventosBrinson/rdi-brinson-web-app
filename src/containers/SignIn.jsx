15c605d3e61d9e8c70776d396430cbb2ae5f083b
David De Anda <omarandstuff@gmail.com>
2017-06-22 21:56:02 -0500

    Clean up

--- src/containers/SignIn.jsx
+++ src/containers/SignIn.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import * as actionCreators from '../action-creators'
 import Immutable from 'immutable'
 
-import { Form, Input, Rate, Radio, Tooltip, Icon, Select, Button, Checkbox } from 'antd'
+import { Form, Input, Icon, Button, Checkbox } from 'antd'
 
 class SignIn extends React.Component {
 
@@ -35,8 +35,7 @@ class SignIn extends React.Component {
   }
 
   render() {
-    let form = this.props.sign_in_form || Immutable.Map()
-    const { getFieldDecorator, isFieldsTouched } = this.props.form
+    const { getFieldDecorator } = this.props.form
 
     return (
       <div style={ { position: 'absolute', maxWidth: '300px', maxHeight: '177px', top: 0, bottom: 0, left: 0, right: 0, margin: 'auto' } }>
@@ -65,35 +64,13 @@ class SignIn extends React.Component {
             })(
               <Checkbox name="remember">Recordarme</Checkbox>
             )}
-            <a href="" style={ { float: 'right' } }>Olvidé mi contraseña</a>
+            <Link to="/recover" style={ { float: 'right' } }>Olvidé mi contraseña</Link>
             <Button type="primary" htmlType="submit" style={ { width: '100%' } }>
               Iniciar seción
             </Button>
           </Form.Item>
         </Form>
       </div>
-    )
-
-    return (
-      <form onSubmit={ this.processSubmit }>
-        <h2>
-          Sign In
-        </h2>
-        <div>
-          <label>Email/Username</label>
-          <input name='credential' type='text' value={ form.get('credential') || '' } onChange={ this.handleChange } />
-        </div>
-        <div>
-          <label>Password</label>
-          <input name='password' type='password' value={ form.get('password') || '' } onChange={ this.handleChange }/>
-        </div>
-        <div>
-          <Link to='/recover'>Forgot your password?</Link>
-        </div>
-        <div>
-          <button type="submit">Sign In</button>
-        </div>
-      </form>
     )
   }
 }
