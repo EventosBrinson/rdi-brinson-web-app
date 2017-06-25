aa08e198e085470fe7766d32bb9ee20fe4ea5dd3
David De Anda <omarandstuff@gmail.com>
2017-06-25 10:26:01 -0500

    Clean up

--- src/containers/places/Edit.jsx
+++ src/containers/places/Edit.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import * as actionCreators from '../../action-creators'
-import * as formHelpers from '../../modules/form-helpers'
 import Immutable from 'immutable'
 
-import { Form, Input, Rate, Radio, Tooltip, Icon, Select, Button } from 'antd'
+import { Form, Input, Button } from 'antd'
 
 const formItemLayout = {
   labelCol: {
