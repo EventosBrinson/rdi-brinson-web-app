38430e70da7e096ab835424a876f66add475a7a6
David De Anda <omarandstuff@gmail.com>
2017-06-29 20:11:19 -0500

    Remove unused code

--- src/modules/form-helpers.js
+++
@@ -1,8 +0,0 @@
-export function priorityValues(values) {
-  for(var i = 0; i < values.length; i++) {
-    if(values[i] === '' || (values[i] !== undefined && values[i] !== null)) {
-      return values[i]
-    }
-  }
-  return ''
-}
