1f427311134a8b6e4e0b499f861a968a8fb25640
David De Anda <omarandstuff@gmail.com>
2017-07-02 20:08:10 -0500

    Rent cast helpers

--- src/modules/cast-helpers.js
+++ src/modules/cast-helpers.js
@@ -1,3 +1,21 @@
 export function address(place) {
   return `${place.get('street')} #${ place.get('outer_number') }${ place.get('inner_number') ? (' Int. ' + place.get('inner_number')) : '' }, ${ place.get('neighborhood') } CP ${ place.get('postal_code') }`
 }
+
+export function fullname(object) {
+  return `${ object.get('firstname') } ${ object.get('lastname') }`
+}
+
+export function priceFormater(value) {
+  var splited = String(value).split('.')
+  var first = '', last = undefined
+
+  if(splited[0]) {
+    first = splited[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  }
+  if(splited[1] !== undefined) {
+    last = splited[1]
+  }
+
+  return `$${first}${last !== undefined ? '.' + (last.length === 1 ? last + '0' : last.slice(-2) ) : '.00' }`
+}
