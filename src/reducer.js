066cd75424f537881c0765f59b74182f97e48718
David De Anda <omarandstuff@gmail.com>
2018-08-08 19:16:07 -0500

    Test INIT regex

--- src/reducer.js
+++ src/reducer.js
@@ -2,28 +2,28 @@ import Immutable from 'immutable'
 import * as core from './core'
 
 export default function reducer(state = Immutable.Map(), action) {
-  console.log(action)
+  if(/@@.*INIT/.test(action.type)){
+      return core.initApp(state)
+  }
+
   switch (action.type) {
-  case '@@redux/INIT':
-  case '@@INIT':
-    return core.initApp(state)
-  case 'CHANGE_FORM':
-    return core.changeForm(state, action.form, action.field, action.value)
-  case 'CLEAN_FORM':
-    return core.cleanForm(state, action.form)
-  case 'MERGE_FORM':
-    return core.mergeForm(state, action.form, action.values)
-  case 'SUBMIT_REQUEST':
-    return core.submitRequest(state, action.request, action.data, action.payload, action.callback)
-  case 'REQUEST_SUCCEEDED':
-    return core.requestSucceeded(state, action.request, action.result, action.payload, action.callback)
-  case 'REQUEST_FAILED':
-    return core.requestFailed(state, action.request, action.result, action.payload, action.callback)
-  case 'CLEAN_ROUTER':
-    return core.cleanRouter(state)
-  case 'CLEAR_STATUS':
-    return core.clearStatus(state, action.statusPath)
-  default:
-    return state
+    case 'CHANGE_FORM':
+      return core.changeForm(state, action.form, action.field, action.value)
+    case 'CLEAN_FORM':
+      return core.cleanForm(state, action.form)
+    case 'MERGE_FORM':
+      return core.mergeForm(state, action.form, action.values)
+    case 'SUBMIT_REQUEST':
+      return core.submitRequest(state, action.request, action.data, action.payload, action.callback)
+    case 'REQUEST_SUCCEEDED':
+      return core.requestSucceeded(state, action.request, action.result, action.payload, action.callback)
+    case 'REQUEST_FAILED':
+      return core.requestFailed(state, action.request, action.result, action.payload, action.callback)
+    case 'CLEAN_ROUTER':
+      return core.cleanRouter(state)
+    case 'CLEAR_STATUS':
+      return core.clearStatus(state, action.statusPath)
+    default:
+      return state
   }
 }
